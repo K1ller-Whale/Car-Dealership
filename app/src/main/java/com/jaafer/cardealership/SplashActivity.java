8ea15f9d7f4e9da099f7c648289d215517d830a7
theHATEM <hatehassel@gmail.com>
2026-01-21 03:34:25 +0300

    Fix bugs and add ListView to MainActivity -There's no comfort in responsibility-

--- app/src/main/java/com/jaafer/cardealership/SplashActivity.java
+++ app/src/main/java/com/jaafer/cardealership/SplashActivity.java
@@ -4,15 +4,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.jaafer.cardealership.database.DatabaseManager;
 import com.jaafer.cardealership.utils.SessionManager;
 
 public class SplashActivity extends AppCompatActivity {
+    private DatabaseManager databaseManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        databaseManager = new DatabaseManager(this);
 
         new Handler().postDelayed(() -> {
+            databaseManager.insertDummyData();
             SessionManager session = new SessionManager(SplashActivity.this);
             Intent intent;
             if (session.isLoggedIn()) {
